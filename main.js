f9df03f456ece184e22ea8b46394d3c2da2d7536
NataliaZgurska <143435353+NataliaZgurska@users.noreply.github.com>
2024-02-28 02:23:17 +0200

    s

---
+++ main.js
@@ -0,0 +1,17 @@
+        //   <div class="hmob-burger-menu">
+        //   <a class="hburger-menu-link" href="">
+        //     <svg class="hburger-menu-icon is-visible" width="28" height="28">
+        //       <use href="../images/icons.svg#icon-burger"></use>
+        //     </svg>
+
+        //    <svg class="hclose-menu-icon" width="28" height="28">
+        //       <use href="../images/icons.svg#icon-x-close"></use>
+        //    </svg>
+        //   </a>
+          
+        //   </div>
+
+
+
+const hBurgerMenu = document.querySelector('.hburger-menu-link');
+console.log(hBurgerMenu);
